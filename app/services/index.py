e02ca8147ae32ba7e2cad460a4876a4835843bc5
alanysrojas <alyfr2000@gmail.com>
2022-08-22 07:02:43 -0500

    Add report in backend and factories

--- app/services/index.py
+++ app/services/index.py
@@ -1,5 +1,7 @@
 from app.common.http_methods import GET
 from flask import Blueprint, jsonify
+
+from ..factories.factories import feed_basic_options
 from ..controllers import IndexController
 
 index = Blueprint('index', __name__)
@@ -8,3 +10,8 @@ index = Blueprint('index', __name__)
 def get_index():
     is_database_up, error = IndexController.test_connection()
     return jsonify({'version': '0.0.2', 'status': 'up' if is_database_up else 'down', 'error': error})
+
+@index.route('/seed_database', methods=GET)
+def seed_database():
+    feed_basic_options()
+    return jsonify({'working': 'hard'})
